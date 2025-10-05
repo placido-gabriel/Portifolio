bfc30c6cd1fc764b6c56d7bf4b154536b9fe9249
Plácido Gabriel <placidogabriel212@gmail.com>
2025-10-05 22:49:04 +0100

    Lorem Ipsum
    Lorem Ipsum dolor sit amet consectectetur adipisicing elit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Olá, meu nome é Plácido Gabriel, seja bem vindo ao meu portifólio pessoal.
---
+++ js/script.js
@@ -0,0 +1,8 @@
+const sidebar = document.getElementById("side-bar")
+function menuPressed(){
+    sidebar.style.display = "flex"
+}
+
+function xPressed(){   
+    sidebar.style.display = "none"
+}
